77d8e7da7cdcfe593c1ae1841319f90ec7f4bc3e
Boyi Qu <boyiqu9596@gmail.com>
2022-11-02 15:47:12 -0700

    created postgres schema file

---
+++ schema.sql
@@ -0,0 +1,46 @@
+\c product;
+
+CREATE TABLE product (
+  id serial PRIMARY KEY,
+  name VARCHAR(255),
+  slogan TEXT,
+  description TEXT,
+  category VARCHAR(50),
+  default_price VARCHAR(50)
+);
+
+CREATE TABLE features (
+  id serial PRIMARY KEY,
+  product_id INT references product(id),
+  feature VARCHAR(255),
+  value VARCHAR(255)
+);
+
+CREATE TABLE styles (
+  id serial PRIMARY KEY,
+  product_id INT references product(id),
+  name VARCHAR(255),
+  sale_price VARCHAR(50),
+  original_price VARCHAR(50),
+  default_style BOOLEAN
+);
+
+CREATE TABLE skus (
+  id serial PRIMARY KEY,
+  styleId INT references styles(id),
+  size VARCHAR(255),
+  quantity INT
+);
+
+CREATE TABLE photos (
+  id serial PRIMARY KEY,
+  styleId INT references styles(id),
+  url TEXT,
+  thumbnail_url TEXT
+);
+
+CREATE TABLE related (
+  id serial PRIMARY KEY,
+  current_product_id INT references product(id),
+  related_product_id INT references product(id),
+)
