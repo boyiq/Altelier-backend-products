301447442f19dcc3f98325b653395e440d16df62
Boyi Qu <boyiqu9596@gmail.com>
2022-11-05 16:00:30 -0700

    added k6 tests for all end points

---
+++ tests/product_test.js
@@ -0,0 +1,23 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+
+export const options = {
+  scenarios:{
+    load_test: {
+      executor: 'per-vu-iterations',
+      vus: 100,
+      iterations: 1,
+      maxDuration: '60s',
+    }
+  }
+};
+
+export default function () {
+  const base_url = 'http://localhost:3001/product';
+  const max = 1000000;
+  const min = 800000;
+  const id = Math.floor(Math.random() * (max - min + 1)) + min;
+  http.get(`${base_url}?id=${id}`);
+  sleep(1);
+}
---
+++ tests/styles_test.js
@@ -0,0 +1,23 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+
+export const options = {
+  scenarios:{
+    load_test: {
+      executor: 'per-vu-iterations',
+      vus: 25,
+      iterations: 1,
+      maxDuration: '2m',
+    }
+  }
+};
+
+export default function () {
+  const base_url = 'http://localhost:3001/styles';
+  const max = 1000000;
+  const min = 800000;
+  const id = Math.floor(Math.random() * (max - min + 1)) + min;
+  http.get(`${base_url}?id=${id}`);
+  sleep(1);
+}
