4a2a6d345273017730683ec6224d6d853b1a8d03
Boyi Qu <boyiqu9596@gmail.com>
2022-11-07 17:29:34 -0800

    cleaned up commented out code

--- server/controllers.js
+++ server/controllers.js
@@ -11,38 +11,6 @@ module.exports = {
       })
   },
 
-/*   getStyles: function (req, res) {
-    let photosQuery = [];
-    let skusQuery = [];
-    let styleidArray = [];
-    let stylesObj = {product_id: req.query.id, results:[]};
-    models.getStyles(req.query.id)
-    .then(({ rows })=>{
-      rows.forEach((row, index)=> {
-        stylesObj.results.push(row);
-        styleidArray.push(row.style_id)
-        skusQuery.push(models.getSkus(row.style_id));
-      });
-      return Promise.all(skusQuery);
-    })
-    .then((response)=>{
-      response.forEach((record, index)=>{
-        stylesObj.results[index].skus = record.rows;
-        photosQuery.push(models.getPhotos(styleidArray[0]));
-      });
-      return Promise.all(photosQuery);
-    })
-    .then((response)=>{
-      console.log(response);
-      response.forEach((record, index)=>{
-        stylesObj.results[index].photos = record.rows;
-      })
-      res.status(200).json(stylesObj)
-    })
-    .catch((err)=>{
-      res.sendStatus(404);
-    })
-  }, */
   getStyles: function (req, res) {
     models.getStyles(req.query.id)
       .then((response)=>{
--- server/models.js
+++ server/models.js
@@ -35,7 +35,18 @@ module.exports = {
                 FROM photos
                 WHERE styleid=styles.id
               )z
-            ) as photos
+            ) as photos,
+            (
+              SELECT json_object_agg(
+                skus.id,
+                (
+                  SELECT json_build_object(
+                    $$size$$, skus.size,
+                    $$quantity$$, skus.quantity
+                  )
+                )
+              ) FROM skus WHERE styleid=styles.id
+            ) as skus
             FROM styles
             WHERE productid=${id}
           )d
@@ -59,28 +70,4 @@ module.exports = {
   getSkus: function(style_id) {
     return db.query(`SELECT * FROM skus WHERE styleid=${style_id}`)
   }
-
-
-/*   getFeatures: function (id) {
-    return db.query(`SELECT feature, value FROM features where product_id=${id}`)
-  },
-
-  getStyles: function(id) {
-    return db.query(`
-      SELECT
-        id as style_id, name, sale_price, original_price, default_style
-      FROM styles where productid=${id}`)
-  },
-
-  getPhotos: function(style_id) {
-    return db.query(`SELECT styleid, thumbnail_url, url FROM photos WHERE styleid=${style_id}`)
-  },
-
-  getSkus: function(style_id) {
-    return db.query(`SELECT styleid, size, quantity FROM skus WHERE styleid=${style_id}`)
-  },
-
-  getRelated: function(id) {
-    return db.query(`SELECT array_agg(related_product_id) FROM related WHERE current_product_id=${id}`)
-  } */
 }
